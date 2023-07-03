b8f9782093de5a99f91dabd398375d6456455ad4
Daniel <daniel.busetto.db@gmail.com>
2023-07-03 17:35:30 +0200

    GenerateMethod

--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,6 +1,16 @@
-import PRODUCTS from "../../../products.json";
-
+import PRODUCTS from "../../products.json";
+import Generation, { generateProductsAndWriteToFile } from "../../example-data";
 export class ProductService {
+
+  async generate(id: number) {
+     Generation(id);
+    return PRODUCTS;
+  }
+
+  async generateTs(id: number) {
+    generateProductsAndWriteToFile(id);
+   return PRODUCTS;
+ }
 
   async find() {
     return PRODUCTS;
