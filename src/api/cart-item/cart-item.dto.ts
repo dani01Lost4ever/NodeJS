a53d6f66459dc5771acc1a245b17e66ee5a42372
Daniel <daniel.busetto.db@gmail.com>
2023-08-02 09:38:51 +0200

    test

--- src/api/cart-item/cart-item.dto.ts
+++ src/api/cart-item/cart-item.dto.ts
@@ -1,16 +1,17 @@
 import { IsInt, Min, Max, IsMongoId, IsOptional } from "class-validator";
-export class AddCartItemDTO{
-    @IsMongoId()
-    productId: string;
-    
-    @IsInt()
-    @Min(1)
-    @Max(1000)
-    quantity: number;
+export class AddCartItemDTO {
+  @IsMongoId()
+  productId: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  quantity: number;
 }
 export class UpdateQuantityDTO {
-    @IsInt()
-    @Min(1)
-    @Max(1000)
-    quantity: number;
-  }
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  quantity: number;
+}
+//test
